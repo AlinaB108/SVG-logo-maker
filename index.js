528a98e2a98a358090208e0303e79c288faa7afb
Alina Badillo <alinachristabel108@gmail.com>
2023-07-26 22:50:43 -0500

    Removed few files and added prompt back in index.js, trying to fix logo error

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
-const CLI = require('./lib/cli.js');
+const inquirer = require('inquirer');
+const fs = require('fs');
+// Need const shapes or not
+const shapes = require('./lib/shapes.js');
 
-const cli = new CLI();
+const generateSVG = ({ text, textcolor, shapecolor })=>
+  `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <${shapes} fill="${shapecolor}" />
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textcolor}">${text}</text>
+  </svg>
+`;
 
-cli.run();
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'text',
+    message: 'Please enter up to three characters (using uppercase).',
+  },
+  {
+    type: 'input',
+    name: 'textcolor',
+    message: 'Please enter a color keyword (OR a hexadecimal number) for the color of text.',
+  },
+  {
+    type: 'list',
+    name: 'shape',
+    message: 'Please choose one of the following shapes',
+    choices: ['circle', 'triangle', 'square'],
+  },
+  {
+    type: 'input',
+    name: 'shapecolor',
+    message: 'Please enter a color keyword (OR a hexadecimal number) for the color of shape.',
+  },
+])
+
+.then((answers) => {
+  const svgLogo = generateSVG(answers);
+
+  fs.writeFile('./lib/logo.svg', svgLogo, (err) =>
+    err ? console.log(err) : console.log('Successfully created SVG logo!')
+  );
+});
