f136efad4b8133a47609946dd2688de1f9e39419
Alina Badillo <alinachristabel108@gmail.com>
2023-07-26 23:44:10 -0500

    Added if statement to create a svg logo, color value is now passed properly

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// Need const shapes or not
-const shapes = require('./lib/shapes.js');
 
-const generateSVG = ({ text, textcolor, shapecolor })=>
+// Imported classes from shapes.js
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
+
+let shapeString = '';
+
+const generateSVG = ({ text, textcolor })=>
   `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <${shapes} fill="${shapecolor}" />
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textcolor}">${text}</text>
-  </svg>
+    ${shapeString}
+    <text x="150" y="125" font-size="50" text-anchor="middle" fill="${textcolor}">${text}</text>
+</svg>
 `;
 
 inquirer
@@ -30,15 +33,35 @@ inquirer
   },
   {
     type: 'input',
-    name: 'shapecolor',
+    name: 'color',
     message: 'Please enter a color keyword (OR a hexadecimal number) for the color of shape.',
   },
 ])
 
 .then((answers) => {
-  const svgLogo = generateSVG(answers);
+
+// If statement to add shape properties based on user's input, it passes it to shapeString
+if (answers.shape === "circle") {
+  shapeChoice = new Circle();
+  shapeString += `<circle cx="150" cy="100" r="80" fill="${answers.color}"/>`;
+}else if (answers.shape === "triangle") {
+  shapeChoice = new Triangle();
+  shapeString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.color}"/>`;
+}else {
+  shapeChoice = new Square();
+  shapeString += `<rect x="50" y="20" width="150" fill="${answers.color}"/>`;
+}
+
+// Passing text, textcolor, shape and color values directly
+const svgLogo = generateSVG({
+  text: answers.text,
+  textcolor: answers.textcolor,
+  shape: shapeString,
+  color: answers.color,
+});
 
   fs.writeFile('./lib/logo.svg', svgLogo, (err) =>
     err ? console.log(err) : console.log('Successfully created SVG logo!')
   );
 });
+
